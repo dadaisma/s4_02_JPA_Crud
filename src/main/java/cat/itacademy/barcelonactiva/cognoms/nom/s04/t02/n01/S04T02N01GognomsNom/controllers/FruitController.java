53c2e5abc8cd3b7cf126ef94183b488e4577d10a
Davide <infodadaisma@gmail.com>
2024-06-11 13:57:40 +0200

    n2trycatch

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNom/controllers/FruitController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNom/controllers/FruitController.java
@@ -3,6 +3,8 @@ package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01GognomsNom
 import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01GognomsNom.model.domain.Fruit;
 import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01GognomsNom.model.services.FruitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,11 +17,17 @@ public class FruitController {
    @Autowired
    private FruitService fruitService;
 
-   @PostMapping("/add")
-    public Fruit addFruit(@RequestBody Fruit fruit){
-       return fruitService.addFruit(fruit);
-   }
-
+    @PostMapping("/add")
+    public ResponseEntity<Fruit> addFruit(@RequestBody Fruit fruit) {
+        try {
+            Fruit addedFruit = fruitService.addFruit(fruit);
+            return new ResponseEntity<>(addedFruit, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // Handle the exception
+            e.printStackTrace(); // Log the exception for debugging purposes (optional)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
    @PutMapping("/update")
     public Fruit updateFruit(@RequestBody Fruit fruit){
        return fruitService.updateFruit(fruit);
