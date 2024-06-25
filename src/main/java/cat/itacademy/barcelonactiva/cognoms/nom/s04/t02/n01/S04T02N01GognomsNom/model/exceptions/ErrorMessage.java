52ffe09f5096ae4330bba59b928baac5cc621f3a
dadaisma <infodadaisma@gmail.com>
2024-06-25 22:20:34 +0200

    addedexceptions

---
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNom/model/exceptions/ErrorMessage.java
@@ -0,0 +1,20 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01GognomsNom.model.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorMessage {
+    private int statusCode;
+    private Date  timestamp;
+    private String message;
+    private String description;
+
+
+
+}
--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNom/model/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01GognomsNom/model/repository/FruitRepository.java
@@ -4,7 +4,9 @@ import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01GognomsNom.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FruitRepository extends JpaRepository<Fruit, Integer> {
-
+    Optional<Fruit> findByName(String name);
 }
